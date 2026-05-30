685e6b451d079520f119554075aa4d618632b112
ShweYaminoo <160458618+ChristineYamin@users.noreply.github.com>
2026-05-30 09:47:17 +0630

    Update\

--- src/2_ocr_engine.py
+++ src/2_ocr_engine.py
@@ -1,4 +1,6 @@
 import easyocr
+import csv
+import os
 
 def extract_text(image_path):
     # Initialize the reader (only need to do this once)
@@ -22,8 +24,24 @@ def extract_text(image_path):
                 print(f"Detected: {text} (Confidence: {prob:.2f})")
     return extracted_data
 
+def save_to_csv(data, output_file="data/ledger_results.csv"):
+    # Define the headers
+    headers = ['Detected_Text', 'Confidence_Score']
+
+    # Save to File
+    with open(output_file, mode='w', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file)
+        writer.writerow(headers)
+        for item in data:
+            writer.writerow([item['text'], f"{item['confidence']:.2f}"])
+    print(f"\nSuccessfully saved results to: {output_file}")
+
 if __name__ == "__main__":
     # Ensure the path matches where the cleaned image is saved
     results = extract_text("data/cleaned_ledger.png")
     print("\n--- Processing Complete ---")
     print(f"Total valid items extraced: {len(results)}")
+    if results:
+        save_to_csv(results)
+    else:
+        print("No valud data found to save.")
